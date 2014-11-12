4e1c196fa4c066a70dd39c31f4f18eb982e65baa
Mauricio Vergara Ereche <mave@cero32.cl>
2014-11-12 10:08:58 -0800

    more options for the display

--- test-map2.py
+++ test-map2.py
@@ -1,4 +1,4 @@
-# brew install matplotlib matplotlib.basemap numpi PyYAML
+# brew install matplotlib matplotlib.basemap numpi PyYAML PIL
 #
 from mpl_toolkits.basemap import Basemap
 import numpy as np
@@ -15,21 +15,36 @@ fig = plt.figure(figsize=(24, 13.5))
 # of the map.
 # lat_ts is the latitude of true scale.
 # resolution = 'c' means use crude resolution coastlines.
-#m = Basemap(projection='merc',llcrnrlat=-58,urcrnrlat=75,\
-#            llcrnrlon=-170,urcrnrlon=192,lat_ts=20,resolution='l')
+#m = Basemap(projection='merc',\
+#			llcrnrlat=-68,\
+#            llcrnrlon=-170,\
+#            urcrnrlat=75,\
+#            urcrnrlon=192,\
+#            lat_ts=20,\
+#            resolution='l')
 
 m = Basemap(projection='robin',lon_0=0, resolution='l')
 
+# Draw topography (doesn't look very good to be honest)
+#m.etopo()
+
+# Draw gray continents. Fill lake with same color as water.
+#m.fillcontinents(color='#e9e9c9', lake_color='#b2d0ff', ax=None, zorder=None, alpha=None)
+m.fillcontinents(color='0.75', lake_color='white', ax=None, zorder=None, alpha=None)
+#m.drawmapboundary(fill_color='#b2d0ff', zorder=None)
+#m.drawmapboundary(color='k', linewidth=0.1, fill_color=None, zorder=None, ax=None)
+
+# Draw relief (better than etopo), then we change water color
+#m.shadedrelief()
+#m.drawlsmask(ocean_color='#b2d0ff',land_color=(255,255,255,0.8))
+
+# draw countries and boundaries
 m.drawcoastlines(linewidth=0.1, linestyle='solid', color='k', antialiased=True)
-m.drawcountries(linewidth=0.1, linestyle='solid', color='k', antialiased=True)
-m.drawmapboundary(color='k', linewidth=0.1, fill_color=None, zorder=None, ax=None)
-#m.drawlsmask(land_color='0.8', ocean_color='w',lakes=False)
-#m.fillcontinents(color='coral',lake_color='aqua')
-m.fillcontinents(color='0.85', lake_color=None, ax=None, zorder=None, alpha=None)
+m.drawcountries(linewidth=0.2, linestyle='solid', color='k', antialiased=True)
 
 # draw parallels and meridians.
-m.drawparallels(np.arange(-90.,91.,10.), linewidth=0.15, zorder=0)
-m.drawmeridians(np.arange(-180.,181.,10.), linewidth=0.15, zorder=0)
+m.drawparallels(np.arange(-90.,91.,10.), linewidth=0.15, zorder=None)
+m.drawmeridians(np.arange(-180.,181.,10.), linewidth=0.15, zorder=None)
 
 data=open('l-root.yml','r')
 rawdata=yaml.load(data)
@@ -45,9 +60,9 @@ for instance in rawdata["Instances"]:
 
 x,y = m(lon,lat)
 
-m.plot(x,y,'b8', markersize=5, zorder=2)
+# Higher zorder so it can be always on the top front layer.
+m.plot(x,y,'b8', markersize=8, zorder=20)
 
-#m.drawmapboundary(fill_color='aqua')
-#plt.title("L-Root Presence")
+plt.title("L-Root Presence")
 plt.show()
 
