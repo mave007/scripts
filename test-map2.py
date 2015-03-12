27a5001b9edb03d4b9bd7110348aa318f3cd08cd
Mauricio Vergara Ereche <mave@cero32.cl>
2015-03-12 10:46:30 -0700

    Latest map version

--- test-map2.py
+++ test-map2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # brew install matplotlib matplotlib.basemap numpi PyYAML PIL
 #
 from mpl_toolkits.basemap import Basemap
@@ -15,12 +16,14 @@ fig = plt.figure(figsize=(24, 13.5))
 # of the map.
 # lat_ts is the latitude of true scale.
 # resolution = 'c' means use crude resolution coastlines.
+#m = Basemap(projection='robin',\
 #m = Basemap(projection='merc',\
 #			llcrnrlat=-68,\
 #            llcrnrlon=-170,\
 #            urcrnrlat=75,\
 #            urcrnrlon=192,\
 #            lat_ts=20,\
+#			lon_0=0,\
 #            resolution='l')
 
 m = Basemap(projection='robin',lon_0=0, resolution='l')
@@ -30,23 +33,26 @@ m = Basemap(projection='robin',lon_0=0, resolution='l')
 
 # Draw gray continents. Fill lake with same color as water.
 #m.fillcontinents(color='#e9e9c9', lake_color='#b2d0ff', ax=None, zorder=None, alpha=None)
-m.fillcontinents(color='0.75', lake_color='white', ax=None, zorder=None, alpha=None)
+#m.fillcontinents(color='0.75', lake_color='white', ax=None, zorder=None, alpha=None)
+#m.fillcontinents(color='0.65', lake_color='#b2d0ff', ax=None, zorder=None, alpha=None)
+
 #m.drawmapboundary(fill_color='#b2d0ff', zorder=None)
 #m.drawmapboundary(color='k', linewidth=0.1, fill_color=None, zorder=None, ax=None)
 
 # Draw relief (better than etopo), then we change water color
-#m.shadedrelief()
-#m.drawlsmask(ocean_color='#b2d0ff',land_color=(255,255,255,0.8))
+m.shadedrelief()
+m.drawlsmask(ocean_color='#b2d0ff',land_color=(255,255,255,0.8))
 
 # draw countries and boundaries
 m.drawcoastlines(linewidth=0.1, linestyle='solid', color='k', antialiased=True)
-m.drawcountries(linewidth=0.2, linestyle='solid', color='k', antialiased=True)
+#m.drawcountries(linewidth=0.2, linestyle='solid', color='k', antialiased=True)
 
 # draw parallels and meridians.
 m.drawparallels(np.arange(-90.,91.,10.), linewidth=0.15, zorder=None)
 m.drawmeridians(np.arange(-180.,181.,10.), linewidth=0.15, zorder=None)
 
 data=open('l-root.yml','r')
+#data=open('root-servers/everyone.yaml','r')
 rawdata=yaml.load(data)
 
 lat=[]
@@ -63,6 +69,6 @@ x,y = m(lon,lat)
 # Higher zorder so it can be always on the top front layer.
 m.plot(x,y,'b8', markersize=8, zorder=20)
 
-plt.title("L-Root Presence")
+#plt.title("Root Servers Presence")
+plt.title("L-Root Server World Presence")
 plt.show()
-
