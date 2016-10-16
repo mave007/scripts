8cafb797962cc8b052d015f8a9a5b79161007196
Mauricio Vergara <mave@cero32.cl>
2016-10-16 12:28:20 -0500

    adding support to v4 and v6 per marcool suggestion

--- rtt-root_servers.sh
+++ rtt-root_servers.sh
@@ -3,19 +3,21 @@ export PATH=/usr/local/opt/coreutils/libexec/gnubin/:/usr/local/bin:/bin:/usr/bi
 
 QRY=". SOA"
 FLAGS="+noall +stats"
+IPV="4 6"
 
+function Query(){
 MIN=9999
 MAX=0
 SUM=0
 CONT=1
-
+ipv="-${1}"
 for roots in {a..m}; do
 	ROOT="${roots}.root-servers.net"
-	ANSt=$(dig ${FLAGS} @${ROOT} ${QRY})
+	ANSt=$(dig ${FLAGS} ${ipv} @${ROOT} ${QRY} 2>/dev/null)
 	if [ $? -eq 0 ] ; then
 		ANS=$(echo ${ANSt} | grep "Query time:" | cut -d " " -f 4)
 		printf " ${ROOT} : %3d\n" ${ANS}
-		CONT=$((CONT+1))
+		CONT=$((CONT+1))	   
 	else
 		echo -e " ${ROOT} : ERROR"
 	fi
@@ -27,3 +29,10 @@ done
 echo "MIN: ${MIN}"
 echo "AVG: $((SUM/CONT))"
 echo "MAX: ${MAX}"
+}
+
+for ipv in ${IPV}; do
+    echo "IPver: ${ipv}"
+	Query ${ipv}
+	echo
+done
