3b1d7bfc81647269bf8cbbe82229a6f9e16eac1f
Mauricio Vergara <mave@cero32.cl>
2017-03-27 12:47:38 -0700

    Removing end-date, version and cutting time from start-date

--- yaml2csv.py
+++ yaml2csv.py
@@ -10,8 +10,16 @@ with open(sys.argv[1], 'r') as stream:
 	docs=yaml.load_all(stream, Loader=yamlordereddictloader.Loader)
 	for doc in docs:
 		for k,v in doc.items():
-			KEY = KEY + str(k) + ","
-			VALUE = VALUE + str(v) + ","
-
+			if str(v) == 'rssac002v3':
+				continue
+			elif str(k) == 'end-period':
+				continue
+			elif str(k) == 'start-period':
+				KEY = KEY + str(k) + ","
+				VALUE = VALUE + str(v)[0:10] + ","
+			else:
+				KEY = KEY + str(k) + ","
+				VALUE = VALUE + str(v) + ","
+			
 print KEY[:-1]
 print VALUE[:-1]
