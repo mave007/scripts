e67fe2994300c91aeeb4accb298e73f7faa6530f
Mauricio Vergara <mave@cero32.cl>
2017-06-08 15:54:26 -0700

    better parsing of time

--- yaml2csv.py
+++ yaml2csv.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
 
 import yaml
-import yamlordereddictloader
 import sys
+import iso8601
 
 KEY=""
-VALUE=""
+VAL=""
 with open(sys.argv[1], 'r') as stream:
-	docs=yaml.load_all(stream, Loader=yamlordereddictloader.Loader)
-	for doc in docs:
-		for k,v in doc.items():
-			if str(v) == 'rssac002v3':
-				continue
-			elif str(k) == 'end-period':
-				continue
-			elif str(k) == 'start-period':
-				KEY = KEY + str(k) + ";"
-				VALUE = VALUE + str(v)[0:10] + ";"
-			else:
-				KEY = KEY + str(k) + ";"
-				VALUE = VALUE + str(v) + ";"
+	docs=yaml.safe_load(stream)
+	for k in iter(sorted(docs.iterkeys())):
+		if "dns" in k: KEY = str(k) + ";" + KEY
+		if "start-period" in k: KEY = str(k) + ";" + KEY
+		if "service" in k: KEY = str(k) + ";" + KEY
+	for k,v in iter(sorted(docs.iteritems())):
+		if "dns" in k: VAL = str(v) + ";" + VAL
+		if "start-period" in k:
+			fecha=iso8601.parse_date(str(v))
+			VAL = str(fecha.isoformat()) + ";" + VAL
+		if "service" in k: VAL = str(v) + ";" + VAL
 			
 print KEY[:-1]
-print VALUE[:-1]
+print VAL[:-1]
