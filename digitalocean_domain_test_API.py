83c16a571f2163c8c4fc85bb71239aa50349ed8a
Mauricio Vergara Ereche <mave@cero32.cl>
2023-03-24 16:37:01 -0700

    Update test funcions

--- digitalocean_domain_test_API.py
+++ digitalocean_domain_test_API.py
@@ -13,6 +13,7 @@ gpg_file='./digitalocean_api_token.txt.gpg'
 with open(gpg_file, 'rb') as f:
     decrypted_data = gpg.decrypt_file(f)
     TOKEN=str(decrypted_data).strip()
+    #print(TOKEN)
 
 # Define the Auth Headers for the API
 headers = {
@@ -20,50 +21,67 @@ headers = {
            'Authorization': 'Bearer ' + TOKEN
 }
 
-# Set up the API endpoint URL
-url = 'https://api.digitalocean.com/v2/domains'
+def AddRR(dom,ip):
+    """
+    This function adds a Resource Record given string `dom` associated to string `ip`
+    
+    Parameters:
+    dom (str): FQDN
+    ip (str): Valid IPv4 Address
+    
+    Returns: 
+    None
+    """
+    
+    data = {
+            'name': dom,
+            'ip_address': ip,
+    }
+    
+    url = 'https://api.digitalocean.com/v2/domains'
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code == 201:
+        print(f'Successfully created domain {dom}')
+    else:
+        print(f'Error creating domain {dom}: {response.status_code} {response.reason}')
 
-# Define the parameters for the new domain
-domain_name = 'test2.do.cero32.cl'
-ip_address = '143.198.73.162'
-
-# Define the data for the new domain
-data = {
-    'name': domain_name,
-    'ip_address': ip_address,
-}
-
-# Make the request to create the new domain
-response = requests.post(url, headers=headers, data=json.dumps(data))
-# Check the response for errors
-if response.status_code == 201:
-    print(f'Successfully created domain {domain_name}')
-else:
-    print(f'Error creating domain {domain_name}: {response.status_code} {response.reason}')
-
-
-######################################
-# Make a request to list all domains
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-    # Loop through list of domains and print their names
-    domains = response.json()['domains']
-    for domain in domains:
-        print(domain['name'])
-else:
-    # Print error message if response was not successful
-    print('Error:', response.status_code, response.text)
+def ListRR():
+    """ 
+    Make a request to list all domains
+    """
+    
+    url = 'https://api.digitalocean.com/v2/domains'
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        # Loop through list of domains and print their names
+        domains = response.json()['domains']
+        for domain in domains:
+            print(domain['name'])
+    else:
+        # Print error message if response was not successful
+        print('Error:', response.status_code, response.text)
 
 
-########################################
-# Delete the domain
-endpoint = f"https://api.digitalocean.com/v2/domains/{domain_name}"
+def DelRR(dom):
+    """
+    Delete the domain given
+    
+    Parameters: 
+    dom (str): FQDN
+    
+    Returns:
+    None
+    """
+    endpoint = f"https://api.digitalocean.com/v2/domains/{dom}"
+    response = requests.delete(endpoint, headers=headers)
+    
+    if response.status_code == 204:
+        print(f"The domain {dom} was successfully deleted.")
+    else:
+        print(f"Failed to delete the domain {domain_name}. Error {response.status_code}: {response.json()['message']}")
 
-# Send the DELETE request
-response = requests.delete(endpoint, headers=headers)
 
-if response.status_code == 204:
-    print(f"The domain {domain_name} was successfully deleted.")
-else:
-    print(f"Failed to delete the domain {domain_name}. Error {response.status_code}: {response.json()['message']}")
+AddRR("sfo.test.do.cero32.cl",'143.198.73.162')
+DelRR("nyc.test.do.cero32.cl")
+AddRR("nyc.test.do.cero32.cl",'138.197.4.218')
